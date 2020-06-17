9a8dea2b23ce950b886a61ed8fbbe862754fefe7
User <kutkg!@gmail.com>
2020-06-18 00:00:53 +0600

    Добавил герои

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,13 +4,14 @@ import java.util.Random;
 
 public class Main {
     public static int counter=0;
-    public static int bossHealth = 700;
+    public static int bossHealth = 800;
     public static int bossDamage = 50;
+    public static int bossDamage2 = bossDamage/5;
     public static String bossDefenceType = "";
-    public static int[] heroesHealth = {260, 250, 270, 200};
-    public static int[] heroesDamage = {20, 15, 25};
+    public static int[] heroesHealth = {260, 250, 270, 200, 150, 185, 220, 230};
+    public static int[] heroesDamage = {20, 15, 25, 0, 10,15,10,25};
     public static String[] heroesAttackType = {"Physical",
-            "Magical", "Kinetic", "Medic"};
+            "Magical", "Kinetic", "Medic", "Golem", "Lucky","Berserk", "Thor"};
 
     public static void main(String[] args) {
         printStatistics();
@@ -42,11 +43,18 @@ public class Main {
             return true;
         }
         if (heroesHealth[0] <= 0 && heroesHealth[1] <= 0 &&
-                heroesHealth[2] <= 0 && heroesHealth[3] <= 0) {
+                heroesHealth[2] <= 0 && heroesHealth[3] <= 0 &&
+                heroesHealth[4] <= 0 && heroesHealth[5] <= 0 &&
+                heroesHealth[6] <= 0 && heroesHealth[7] <= 0) {
             System.out.println("Boss won!!!");
             return true;
         }
         return false;
+    }
+    public static void dodge(){
+        Random L = new Random();
+        int num5 = L.nextInt(1);
+
     }
 
     public static void printStatistics() {
@@ -56,6 +64,10 @@ public class Main {
         System.out.println("Magic health: " + heroesHealth[1]);
         System.out.println("Kinetic health: " + heroesHealth[2]);
         System.out.println("Medic health: " + heroesHealth[3]);
+        System.out.println("Golem health: " + heroesHealth[4]);
+        System.out.println("Lucky health: " + heroesHealth[5]);
+        System.out.println("Berserk health: " + heroesHealth[6]);
+        System.out.println("Thor health: " + heroesHealth[7]);
         System.out.println("Раунд "+counter++);
         System.out.println("________________");
     }
@@ -63,7 +75,10 @@ public class Main {
     public static void bossHits() {
         for (int i = 0; i < heroesHealth.length; i++) {
             if (heroesHealth[i] > 0) {
-                if (heroesHealth[i] - bossDamage < 0) {
+                if (heroesHealth[i] - bossDamage < 0 ) {
+                    if (heroesHealth[i] == heroesHealth[4] && heroesHealth[4] > 0){
+                        heroesHealth[4] = - bossDamage2;
+                    }
                     heroesHealth[i] = 0;
                 } else {
                     heroesHealth[i] = heroesHealth[i] - bossDamage;
@@ -77,9 +92,9 @@ public class Main {
         int x = num.nextInt(heroesDamage.length);
         int z = num2.nextInt(30)+5;
         if (heroesHealth[x] < 100 && heroesHealth[3] > 0){
-        heroesHealth[x] = heroesHealth[x]+ z;
+        heroesHealth[x] = heroesHealth[x] + z;
         }
-        System.out.println("Random выбрал индекс " + x);
+        System.out.println("Медик выбрал " + x);
     }
 
 
